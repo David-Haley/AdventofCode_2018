2aa93de263391b53995443611300cd3317d00f9a
David-Haley <34273510+David-Haley@users.noreply.github.com>
2022-04-16 01:11:57 +1000

    Update december_24.adb
    Gets correct answer for part one but not for part two. Part not checked against example. Likely some error in "tie breaking"

--- december_24.adb
+++ december_24.adb
@@ -1,5 +1,6 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Command_Line; use Ada.Command_Line;
+with Ada.Exceptions; use Ada.Exceptions;
 with Ada.Text_IO.Unbounded_IO; use Ada.Text_IO.Unbounded_IO;
 with Ada.Strings; use Ada.Strings;
 with Ada.Strings.Maps; use Ada.Strings.Maps;
@@ -58,6 +59,7 @@ procedure December_24 is
 
 
    begin -- Get_Input
+      Group_Store := Group_Stores.Empty_Map;
       if Argument_Count = 0 then
          Open (Input_File, In_File, "december_24.txt");
       else
@@ -70,8 +72,7 @@ procedure December_24 is
          elsif text = "Infection:" then
             Group.Army := Infection;
          else
-            raise Input_Error with Positive_Count'Image (Line (Input_File) - 1)
-              & ": Unknown group type:" & To_String (Text);
+            raise Input_Error with ": Unknown group type:" & To_String (Text);
          end if; -- Text = "Immune System:"
          Get_Line (Input_File, Text);
          while Length (Text) /= 0 loop
@@ -86,9 +87,7 @@ procedure December_24 is
             Start_At := Last + 1;
             End_Hit := Index (Text, Hit_String, Start_At);
             if End_Hit = 0 then
-               raise Input_Error with
-               Positive_Count'Image (Line (Input_File) - 1) & ": " &
-                 Hit_String & " not found";
+               raise Input_Error with Hit_String & " not found";
             end if; -- End_Hit = 0
             End_Hit := End_Hit + Hit_String'Length;
             Property_Start := Index (Text, Weak_String, End_Hit);
@@ -116,10 +115,8 @@ procedure December_24 is
             Start_At := Last + 1;
             Find_Token (Text, Space_Set, Start_At, Outside, First, Last);
             if Slice (Text, First, Last) /= Damage_String then
-               raise Input_Error with
-               Positive_Count'Image (Line (Input_File) - 1) & ": expected " &
-                 Damage_String & " and found """ & Slice (Text, First, Last) &
-               '"';
+               raise Input_Error with "Expected " & Damage_String &
+                 " and found """ & Slice (Text, First, Last) & '"';
             end if; -- Slice (Text, First, Last) /= Damage_String
             Start_At := Last + 1;
             Find_Token (Text, Decimal_Digit_Set, Start_At, Inside, First, Last);
@@ -134,6 +131,11 @@ procedure December_24 is
          end loop; -- Length (Text) /= 0
       end loop; -- not End_Of_File (Input_File
       Close (Input_File);
+   exception
+      when E: others =>
+         Put_Line (Positive_Count'Image (Line (Input_File) - 1) & ": " &
+                     Exception_Message (E));
+         raise;
    end Get_Input;
 
    procedure Put (Group_Store : in Group_Stores.Map) is
@@ -159,20 +161,19 @@ procedure December_24 is
    procedure Select_Targets (Group_Store : in Group_Stores.Map;
                              Attack_List : out Attack_Lists.Map) is
 
-      type Group_Prioruties is record
+      type Group_Priorities is record
          Effective_Power, Initiative : Values;
-      end record; -- Group_Prioruties;
+      end record; -- Group_Priorities;
 
-      function "<" (Left, Right : Group_Prioruties) return Boolean is
+      function "<" (Left, Right : Group_Priorities) return Boolean is
 
       begin -- "<"
          return Left.Effective_Power < Right.Effective_Power or
            (Left.Effective_Power = Right.Effective_Power and
               Left.Initiative > Right.Initiative);
-         -- When effective power is tied higher initiative goes first.
       end "<";
 
-      function "=" (Left, Right : Group_Prioruties) return Boolean is
+      function "=" (Left, Right : Group_Priorities) return Boolean is
 
       begin -- "="
          return Left.Effective_Power = Right.Effective_Power and
@@ -180,7 +181,7 @@ procedure December_24 is
       end "=";
 
       package Choice_Lists is new
-        Ada.Containers.Ordered_Maps (Group_Prioruties, Group_Indices);
+        Ada.Containers.Ordered_Maps (Group_Priorities, Group_Indices);
       use Choice_Lists;
 
       type Target_Priorities is record
@@ -207,6 +208,7 @@ procedure December_24 is
 
       package Target_Lists is new
         Ada.Containers.Ordered_Maps (Target_Priorities, Group_Indices);
+      use Target_Lists;
 
       package Target_Sets is new Ada.Containers.Ordered_Sets (Group_Indices);
       use Target_Sets;
@@ -214,6 +216,8 @@ procedure December_24 is
       Target_Set : Target_Sets.Set := Target_Sets.Empty_Set;
       Choice_List : Choice_Lists.Map := Choice_Lists.Empty_Map;
       A : Group_Indices;
+      Target_Priority : Target_Priorities;
+      Target_List : Target_Lists.Map := Target_Lists.Empty_Map;
 
    begin -- Select_Targets
       Attack_List := Attack_Lists.Empty_Map;
@@ -223,26 +227,120 @@ procedure December_24 is
                  (Group_Store (G).Quantity * Group_Store (G).Damage,
                   Group_Store (G).Initiative), Key (G));
       end loop; -- G in Iterate (Group_Store)
-      for C in Iterate (Choice_List) loop
+      for C in reverse Iterate (Choice_List) loop
          A := Choice_List (C); -- Group index of attacker
+         Clear (Target_List);
          for D in Iterate (Group_Store) loop
             if Group_Store (A).Army /= Group_Store (D).Army and
               not Contains (Group_Store (D).Immunity,
-                            Group_Store (A).Damage_Type) then
+                            Group_Store (A).Damage_Type) and
+              not Contains (Target_Set, Key (D)) then
                -- Opposing armies and not immune
-               null;
+               if (Contains (Group_Store (D).Weakness,
+                             Group_Store (A).Damage_Type)) then
+                  Target_Priority.Damage := Group_Store (A).Quantity *
+                    Group_Store (A).Damage * 2;
+               else
+                  Target_Priority.Damage := Group_Store (A).Quantity *
+                    Group_Store (A).Damage;
+               end if; -- (Contains (Group_Store (D).Weakness, ...
+               Target_Priority.Effective_Power :=
+                 Group_Store (D).Quantity * Group_Store (D).Damage;
+               Target_Priority.Initiative := Group_Store (D).Initiative;
+               insert (Target_List, Target_Priority, Key (D));
             end if; -- Group_Store (A).Army /= Group_Store (D).Army and ...
          end loop; -- D in Iterate (Group_Store)
+         if not Is_Empty (Target_List) then
+            Insert (Target_Set, Last_Element (Target_List));
+            insert (Attack_List, Group_Store (A).Initiative,
+                    (A, Last_Element (Target_List)));
+         end if; -- not Is_Empty (Target_List)
       end loop; -- A in Iterate (Choice_List)
    end Select_Targets;
 
+   procedure Battle (Group_Store : in out Group_Stores.Map;
+                     Attack_List : in Attack_Lists.Map;
+                     No_Change : out Boolean) is
+
+      A, D : Group_Indices;
+      Destroys : Natural;
+
+   begin -- Battle
+      No_Change := True;
+      for I in reverse Iterate (Attack_List) loop
+         -- in order of decreasing initiative
+         if Contains (Group_Store, Attack_List (I).Attacker) then
+            -- It is possible for an attacker to no longer exist if it was the
+            -- the target of an earlier attack. The defender must still exixt as
+            -- only one group is permmitted to attack in each round.
+            A := Attack_List (I).Attacker;
+            D := Attack_List (I).Defender;
+            if (Contains (Group_Store (D).Weakness,
+                          Group_Store (A).Damage_Type)) then
+               Destroys := Group_Store (A).Quantity * Group_Store (A).Damage * 2
+                 / Group_Store (D).Hit;
+            else
+               Destroys := Group_Store (A).Quantity * Group_Store (A).Damage
+                 / Group_Store (D).Hit;
+            end if; -- (Contains (Group_Store (D).Weakness, ...
+                    -- integer division reaults in floor
+            No_Change := No_Change and Destroys = 0;
+            -- no effective attack
+            if Destroys >= Group_Store (D).Quantity then
+               Delete (Group_Store, D);
+            else
+               Group_Store (D).Quantity := Group_Store (D).Quantity - Destroys;
+            end if; -- Destroys >= Group_Store (D).Quantity
+         end if; -- Contains (Group_Store, Attack_List (I).Attacker)
+      end loop; -- Al in Iterate (Attack_List)
+   end Battle;
+
    Group_Store : Group_Stores.Map := Group_Stores.Empty_Map;
    Attack_List : Attack_Lists.Map;
+   Unit_Count : Natural;
+   No_Change : Boolean;
+   Boost : Values := 1;
 
 begin -- December_24
    Get_Input (Group_Store);
    Put (Group_Store);
-   Select_Targets (Group_Store, Attack_List);
-   Put_Line ("Part one:");
+   loop -- until nothing left to attack
+      Select_Targets (Group_Store, Attack_List);
+      exit when Is_Empty (Attack_List);
+      Battle (Group_Store, Attack_List, No_Change);
+   end loop; -- until nothing left to attack
+   Unit_Count := 0;
+   for G in Iterate (Group_Store) loop
+      Unit_Count := Unit_Count + Group_Store (G).Quantity;
+   end loop; -- G in Iterate (Group_Store)
+   Put_Line ("Part one, remaining units:" & Unit_Count'Img);
+   Put_CPU_Time;
+   loop -- until minimum boost
+      Get_Input (Group_Store);
+      for G in Iterate (Group_Store) loop
+         if Group_Store (G).Army = Immune_System then
+            Group_Store (G).Damage := Group_Store (G).Damage + Boost;
+         end if; -- Group_Store (G).Army = Immune_System
+      end loop; -- G in Iterate (Group_Store)
+      No_Change := False;
+      -- provide initial value so there is atleast one battle
+      loop -- until nothing left to attack
+         Select_Targets (Group_Store, Attack_List);
+         exit when Is_Empty (Attack_List) or No_Change;
+         Battle (Group_Store, Attack_List, No_Change);
+      end loop; -- until nothing left to attack
+      Unit_Count := 0;
+      if Is_Empty (Attack_List) then
+         -- ended with one army having no units left
+         for G in Iterate (Group_Store) loop
+            if Group_Store (G).Army = Immune_System then
+               Unit_Count := Unit_Count + Group_Store (G).Quantity;
+            end if; -- Group_Store (G).Army = Immune_System
+         end loop; -- G in Iterate (Group_Store)
+      end if; -- Is_Empty (Attack_List)
+      exit when Unit_Count > 0;
+      Boost := Boost + 1;
+   end loop; -- until minimum boost
+   Put_Line ("Part two, remaining immune system units:" & Unit_Count'Img);
    Put_CPU_Time;
 end December_24;
